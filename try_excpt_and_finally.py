7d3ca247dc329373b692d07209da92f34c9eb49d
2115001121 <93867695+Vineetkumarsingh12@users.noreply.github.com>
2022-05-26 19:19:36 +0530

    Create try_excpt_and_finally.py

---
+++ try_excpt_and_finally.py
@@ -0,0 +1,11 @@
+num1=int(input())
+num2=int(input())
+try:
+    print("the sum of these two numbers is ",
+    num1+num2)
+    print("the sum of these two numbers is ",
+    num1+num23)
+except Exception as g:
+    print(g)   
+finally:      
+    print("this line is  very important")    
